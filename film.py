f5463f113468525525abc348288117e0b9ca3117
roshanjossey <roshanjossey@gmail.com>
2015-09-02 23:56:11 +0530

    exporting to text done

--- film.py
+++ film.py
@@ -15,6 +15,23 @@ class Film(object):
 		self.lead_actor = lead_actor
 		self.genre = genre
 
+
+class ExportToText(object):
+	"""Exports details of film to text file"""
+	def __init__(self, film):
+		_file = open('film_details.txt', 'w')
+		_file.write(
+			'''Name: {0} \n
+			   Run time: {1} \n
+			   Language: {2} \n
+			   Lead Actor: {3} \n
+			   Genre: {4} \n'''.format(
+			   	                       film.name,
+			   	                       film.run_time,
+			   	                       film.language,
+			   	                       film.lead_actor,
+			   	                       film.genre))
+		
 def main():
 	print 'Enter name of film:'
 	name = raw_input()
@@ -28,6 +45,11 @@ def main():
 	genre = raw_input()
 	movie = Film(name, run_time, language, lead_actor, genre)
 	print movie.name, movie.run_time, movie.language, movie.lead_actor, movie.genre
+	print 'Which format should the data be converted to?'
+	print '    1. Text\n    2. Pdf\n    3. Don\'t sweat it\n Enter your option'
+	option = input()
+	if option == 1:
+		ExportToText(movie)
 
 if __name__ == '__main__':
 	main()
